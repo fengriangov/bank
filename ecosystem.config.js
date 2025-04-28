d751e67c79bd063004b10efdc5ce908a3fbf302b
Jasper Green <99476285+mcl-playz@users.noreply.github.com>
2025-04-28 12:24:58 +0800

    Added pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+        name: "fengri-bank",
+        script: "server.js",
+        instances: "max",
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "500M",
+        env_production: {
+            NODE_ENV: "production"
+        }
+    }]
+};
